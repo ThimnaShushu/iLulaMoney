f78841e99a3454559d63e6ef1edc3dd9d79e232c
ThimnaShushu <thimnashushu@gmail.com>
2025-06-28 09:13:41 +0200

    the client has the wallet addresses for sending and receiving the money.

--- client.js
+++ client.js
@@ -4,3 +4,13 @@ const net = require('net');
 const client = new net.Socket();
 const HOST = '196.42.66.161';
 const PORT = 3003;
+
+client.connect(PORT, HOST, () =>{
+    console.log('Connected to server');
+
+    
+  
+    const sendWallet = "$ilp.interledger-test.dev/thimzar";
+    const receiverWallet = "$ilp.interledger-test.dev/thimusd";
+    const amountMoney = "300";
+})
